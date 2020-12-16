193e8411472a09eaad1bc4be72d1f399657d85b0
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-12-16 17:31:19 +0100

    fix formatting

--- pytest_sentry.py
+++ pytest_sentry.py
@@ -12,21 +12,21 @@ from sentry_sdk.scope import add_global_event_processor
 
 _ENVVARS_AS_TAGS = frozenset(
     [
-        "GITHUB_WORKFLOW",  #  The name of the workflow.
-        "GITHUB_RUN_ID",  #  A unique number for each run within a repository. This number does not change if you re-run the workflow run.
-        "GITHUB_RUN_NUMBER",  #  A unique number for each run of a particular workflow in a repository. This number begins at 1 for the workflow's first run, and increments with each new run. This number does not change if you re-run the workflow run.
-        "GITHUB_ACTION",  #  The unique identifier (id) of the action.
-        "GITHUB_ACTOR",  #  The name of the person or app that initiated the workflow. For example, octocat.
-        "GITHUB_REPOSITORY",  #  The owner and repository name. For example, octocat/Hello-World.
-        "GITHUB_EVENT_NAME",  #  The name of the webhook event that triggered the workflow.
-        "GITHUB_EVENT_PATH",  #  The path of the file with the complete webhook event payload. For example, /github/workflow/event.json.
-        "GITHUB_WORKSPACE",  #  The GitHub workspace directory path. The workspace directory is a copy of your repository if your workflow uses the actions/checkout action. If you don't use the actions/checkout action, the directory will be empty. For example, /home/runner/work/my-repo-name/my-repo-name.
-        "GITHUB_SHA",  #  The commit SHA that triggered the workflow. For example, ffac537e6cbbf934b08745a378932722df287a53.
-        "GITHUB_REF",  #  The branch or tag ref that triggered the workflow. For example, refs/heads/feature-branch-1. If neither a branch or tag is available for the event type, the variable will not exist.
-        "GITHUB_HEAD_REF",  #  Only set for pull request events. The name of the head branch.
-        "GITHUB_BASE_REF",  #  Only set for pull request events. The name of the base branch.
-        "GITHUB_SERVER_URL",  #  Returns the URL of the GitHub server. For example: https://github.com.
-        "GITHUB_API_URL",  #  Returns the API URL. For example: https://api.github.com.
+        "GITHUB_WORKFLOW",  # The name of the workflow.
+        "GITHUB_RUN_ID",  # A unique number for each run within a repository. This number does not change if you re-run the workflow run.
+        "GITHUB_RUN_NUMBER",  # A unique number for each run of a particular workflow in a repository. This number begins at 1 for the workflow's first run, and increments with each new run. This number does not change if you re-run the workflow run.
+        "GITHUB_ACTION",  # The unique identifier (id) of the action.
+        "GITHUB_ACTOR",  # The name of the person or app that initiated the workflow. For example, octocat.
+        "GITHUB_REPOSITORY",  # The owner and repository name. For example, octocat/Hello-World.
+        "GITHUB_EVENT_NAME",  # The name of the webhook event that triggered the workflow.
+        "GITHUB_EVENT_PATH",  # The path of the file with the complete webhook event payload. For example, /github/workflow/event.json.
+        "GITHUB_WORKSPACE",  # The GitHub workspace directory path. The workspace directory is a copy of your repository if your workflow uses the actions/checkout action. If you don't use the actions/checkout action, the directory will be empty. For example, /home/runner/work/my-repo-name/my-repo-name.
+        "GITHUB_SHA",  # The commit SHA that triggered the workflow. For example, ffac537e6cbbf934b08745a378932722df287a53.
+        "GITHUB_REF",  # The branch or tag ref that triggered the workflow. For example, refs/heads/feature-branch-1. If neither a branch or tag is available for the event type, the variable will not exist.
+        "GITHUB_HEAD_REF",  # Only set for pull request events. The name of the head branch.
+        "GITHUB_BASE_REF",  # Only set for pull request events. The name of the base branch.
+        "GITHUB_SERVER_URL",  # Returns the URL of the GitHub server. For example: https://github.com.
+        "GITHUB_API_URL",  # Returns the API URL. For example: https://api.github.com.
     ]
 )
 
