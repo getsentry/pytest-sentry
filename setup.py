3463f804c0e645b81eda130aece4ff0e0e9ea16d
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2022-11-07 11:18:32 -0500

    setup-py-upgrade + setup-cfg-fmt (#16)

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
 from setuptools import setup
-
-setup(
-    name="pytest-sentry",
-    description="A pytest plugin to send testrun information to Sentry.io",
-    long_description=open("README.rst").read(),
-    author="Markus Unterwaditzer",
-    version="0.1.9",
-    license="BSD",
-    author_email="markus@unterwaditzer.net",
-    url="https://github.com/untitaker/pytest-sentry",
-    py_modules=["pytest_sentry"],
-    entry_points={"pytest11": ["sentry = pytest_sentry"]},
-    install_requires=["pytest", "sentry-sdk", "wrapt"],
-)
+setup()
