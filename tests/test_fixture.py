2ea3687c665c784eee2b00c59e6ca9651f033a4e
Daniel Szoke <szokeasaurusrex@users.noreply.github.com>
2024-04-04 16:29:19 +0200

    feat: SDK 2.0 Support (#31)
    This commit enables support for 2.0 of the Sentry SDK. The changes are likely incompatible with Sentry SDK 1.x.
    
    Fixes https://github.com/getsentry/pytest-sentry/issues/27

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -7,10 +7,10 @@ GLOBAL_CLIENT = pytest_sentry.Client()
 pytestmark = pytest.mark.sentry_client(GLOBAL_CLIENT)
 
 
-def test_basic(sentry_test_hub):
-    assert sentry_test_hub.client is GLOBAL_CLIENT
+def test_basic(sentry_test_scope):
+    assert sentry_test_scope.client is GLOBAL_CLIENT
 
 
 @pytest.mark.sentry_client(None)
-def test_func(sentry_test_hub):
-    assert sentry_test_hub.client is None
+def test_func(sentry_test_scope):
+    assert not sentry_test_scope.client.is_active()
